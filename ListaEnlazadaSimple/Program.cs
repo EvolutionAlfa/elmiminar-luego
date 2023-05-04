8df2130eedbc9d04160a8f4d94e93c9542a488c0
EvolutionAlfa <enocr135@gmail.com>
2023-05-04 06:02:55 -0500

    agregar mas ususarios

--- ListaEnlazadaSimple/Program.cs
+++ ListaEnlazadaSimple/Program.cs
@@ -5,6 +5,7 @@ Locomotora loco = new Locomotora();
 loco.AgregarInicio("Doro");
 loco.AgregarInicio("Vago");
 loco.AgregarInicio("Vegeto");
-
+loco.AgregarInicio("individuo");
+loco.AgregarInicio2("vecino");
 loco.AgregarInicio("Kawai");
 Console.WriteLine(loco.Imprimir());
